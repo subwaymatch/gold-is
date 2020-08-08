f6c61d6b4722acf78a2fb5c29997d7b2b33666f7
Ye Joo Park <subwaymatch@gmail.com>
2020-08-08 17:28:47 -0500

    Convert Monaco to a controlled editor, add MIT license

--- components/monaco-editor.tsx
+++ components/monaco-editor.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import dynamic from 'next/dynamic';
+const ControlledEditor = dynamic(
+  import('@monaco-editor/react').then((mod) => mod.ControlledEditor),
+  {
+    ssr: false,
+  }
+);
 
-const Editor = dynamic(import('@monaco-editor/react'), { ssr: false });
+type MonacoEditorComponentProps = {
+  defaultCode: string;
+};
 
-export default function MonacoEditorComponent(_) {
+export default function MonacoEditorComponent({
+  defaultCode,
+}: MonacoEditorComponentProps) {
+  const [editorValue, setEditorValue] = useState(defaultCode);
+
+  const handleEditorChange = (ev, value) => {
+    console.log(`handleEditorChange`);
+    console.log(ev);
+    console.log(value);
+
+    setEditorValue(value);
+  };
+
   return (
     <div style={{ backgroundColor: '#fc3' }}>
-      <Editor height="30vh" language="python" />
+      <ControlledEditor
+        height="40vh"
+        value={editorValue}
+        onChange={handleEditorChange}
+        language="python"
+        options={{
+          folding: false,
+          fontSize: 19,
+          wordWrap: 'on',
+          minimap: {
+            enabled: false,
+          },
+          className: 'gold-editor',
+          wrapperClassName: 'gold-editor-wrapper',
+        }}
+      />
     </div>
   );
 }
