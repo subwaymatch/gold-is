2e1a321466da6fb5e3b26e4fde12608543ec7904
Ye Joo Park <subwaymatch@gmail.com>
2020-08-29 08:02:26 -0500

    Remove unused example Zustand store

--- bear-store.ts
+++
@@ -1,7 +0,0 @@
-import create from 'zustand';
-
-export const useBearStore = create((set) => ({
-  bears: 0,
-  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-  removeAllBears: () => set({ bears: 0 }),
-}));
